50edf65eb1f9024b9ce0fc1adf89a4db9936d76d
ClaudioLeone <claleone92@gmail.com>
2023-05-20 16:13:35 +0200

    Created 'Category' model, linked Font Awesome

---
+++ Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+class Category{
+    
+    function __construct(public $name, public $icon)
+    {
+        $this->name = $name;
+        $this->icon = $icon;
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,18 @@
+<!-- 
+    Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+        - L'e-commerce vende **prodotti** per animali. I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.
+        - I prodotti saranno oltre al **cibo**, anche **giochi**, **cucce**, etc.
+
+    Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria, tipo
+    di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+-->
+
 <?php
+require_once __DIR__ . "/Models/Category.php";
+
+$kiwibird_category = new Category("kiwibird", "fa-solid fa-kiwi-bird");
+$dog_category = new Category("dog", "fa-solid fa-dog");
+
 
 ?>
 
@@ -10,12 +24,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <title>BoolPets</title>
 </head>
 <body>
     <div id="page">
         <h1>TEST</h1>
+        <h4><?php var_dump($dog_category); ?></h4>
+
     </div>
 </body>
 </html>
